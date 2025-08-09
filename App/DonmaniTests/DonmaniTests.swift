96bfb47dcff548b84a83e76f43dc4f8a619a52c0
moonjs0113 <littlemoom@naver.com>
2025-08-09 15:57:40 +0900

    Fix VersionManager Test Code

--- App/DonmaniTests/DonmaniTests.swift
+++ App/DonmaniTests/DonmaniTests.swift
@@ -10,32 +10,6 @@ import Testing
 
 struct DonmaniTests {
 
-    @Test func compareVersionEqual() async throws {
-        let versionManager = VersionManager()
-        let currentVersion = "1.17.0"
-        let storeVersion = "1.17.0"
-        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
-    }
-    
-    @Test func compareVersionLowerMajor() async throws {
-        let versionManager = VersionManager()
-        let currentVersion = "1.17.3"
-        let storeVersion = "2.2.0"
-        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
-    }
-    
-    @Test func compareVersionLowerMinor() async throws {
-        let versionManager = VersionManager()
-        let currentVersion = "1.17.0"
-        let storeVersion = "1.16.0"
-        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
-    }
-    
-    @Test func compareVersionLowerBuildNumber() async throws {
-        let versionManager = VersionManager()
-        let currentVersion = "1.17"
-        let storeVersion = "1.17.3"
-        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
-    }
+
     
 }
---
+++ App/DonmaniTests/VersionManagerTests.swift
@@ -0,0 +1,60 @@
+//
+//  VersionManagerTests.swift
+//  DonmaniTests
+//
+//  Created by 문종식 on 8/9/25.
+//
+
+import Testing
+@testable import Donmani
+
+struct VersionManagerTests {
+    let versionManager = VersionManager()
+    
+    @Test func compare_equal_version() async throws {
+        let currentVersion = "1.17.0"
+        let storeVersion = "1.17.0"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
+    }
+    
+    @Test func compare_lower_major_version() async throws {
+        let currentVersion = "1.17.3"
+        let storeVersion = "2.2.0"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
+    }
+    
+    @Test func compare_lower_minor_version() async throws {
+        let currentVersion = "1.17.0"
+        let storeVersion = "1.16.0"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
+    }
+    
+    @Test func compare_lower_build_version() async throws {
+        let currentVersion = "1.17"
+        let storeVersion = "1.17.3"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
+    }
+    
+    @Test
+    func compare_upper_major_version() async throws {
+        let currentVersion = "1.17.3"
+        let storeVersion = "2.0.0"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
+    }
+
+    @Test
+    func compare_upper_minor_version() async throws {
+        let currentVersion = "1.17.0"
+        let storeVersion = "1.18.0"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
+    }
+
+    @Test
+    func compare_upper_build_version() async throws {
+        let currentVersion = "1.17.0"
+        let storeVersion = "1.17.3"
+        #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
+    }
+
+
+}

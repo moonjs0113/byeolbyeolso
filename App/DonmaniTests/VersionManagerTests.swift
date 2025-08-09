135308df550eebc7098d4d31c3a51a31f97ae03e
moonjs0113 <littlemoom@naver.com>
2025-08-09 15:58:03 +0900

    Fix VersionManager Test Code

--- App/DonmaniTests/VersionManagerTests.swift
+++ App/DonmaniTests/VersionManagerTests.swift
@@ -11,25 +11,29 @@ import Testing
 struct VersionManagerTests {
     let versionManager = VersionManager()
     
-    @Test func compare_equal_version() async throws {
+    @Test
+    func compare_equal_version() async throws {
         let currentVersion = "1.17.0"
         let storeVersion = "1.17.0"
         #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
     }
     
-    @Test func compare_lower_major_version() async throws {
+    @Test
+    func compare_lower_major_version() async throws {
         let currentVersion = "1.17.3"
         let storeVersion = "2.2.0"
         #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
     }
     
-    @Test func compare_lower_minor_version() async throws {
+    @Test
+    func compare_lower_minor_version() async throws {
         let currentVersion = "1.17.0"
         let storeVersion = "1.16.0"
         #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == true)
     }
     
-    @Test func compare_lower_build_version() async throws {
+    @Test
+    func compare_lower_build_version() async throws {
         let currentVersion = "1.17"
         let storeVersion = "1.17.3"
         #expect(versionManager.isLastestVersion(store: storeVersion, current: currentVersion) == false)
